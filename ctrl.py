9185d9d4929fcc1b90e431f7f7f8120dce934371
cbk0605 <cbk0605@gmail.com>
2025-07-01 16:53:23 +0900

    Add mod function and modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -22,11 +22,22 @@ class Control:
                 return f'{num1} / {num2} = {self.div(num1, num2)}'
             elif operator == '^':
                 return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+            elif operator == '%':
+                return f'{num1} % {num2} = {self.mod(num1, num2)}'
             else:
                 return 'Calculation Error'
         except:
             return 'Calculation Error'
         
+    def mod(self, a, b):    # 나머지 연산 함수 추가
+        try:
+            if (b==0):  # 0으로 나누는 경우 예외 처리
+                raise Exception("Divisor Error")  
+        except Exception as e:
+            return e
+        
+        return a % b
+    
     def connectSignals(self):   # 시그널 연결
         self.view.btn1.clicked.connect(lambda:self.view.setDisplay(self.calculate()))  
         self.view.btn2.clicked.connect(self.view.clearMessage)
